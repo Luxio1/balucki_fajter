c8b1c0eabf3413cf5b87543aee87d24fd365ff7e
Patryk Orłowski <222530@edu.p.lodz.pl>
2020-11-08 23:02:25 +0100

    Added events to game window

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include <SFML/Window.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
 #include "Camera.h"
 
 using namespace cv;
@@ -12,8 +13,39 @@ string filePath = "/home/luxio/Desktop/balucki_fajter/SamplePhotos/simple/rekawi
 //sf::Window window(sf::VideoMode(800, 600), "Balucki fajter");
 
 int main( int argc, char** argv ) {
-    auto* camera = new Camera(filePath, isPhoto);
 
+    //Game Window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Balucki fajter");
+    sf::Event event;
+
+    //Game loop
+    while(window.isOpen()) {
+        //Event polling (to event variable)
+        while (window.pollEvent(event)){
+            switch (event.type) {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+                case sf::Event::KeyPressed:
+                    if(event.key.code == sf::Keyboard::Escape){
+                        window.close();
+                    }
+                    break;
+            }
+        }
+    }
+
+    //Update
+
+
+    //Render
+    window.clear();
+
+    //Draw game
+    window.display();
+
+    //Photo and camera
+    auto* camera = new Camera(filePath, isPhoto);
     if(camera->isPhoto()){
         camera->runWithPhoto();
     } else {
