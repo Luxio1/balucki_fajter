d5848c015427997ea4a0e4bd8cff4043a64f1eea
Bartłomiej Abel <222391@edu.p.lodz.pl>
2020-11-09 17:08:10 +0100

    updated the framerate

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -235,116 +235,109 @@ void Camera::detectRed() {
 }
 
 void Camera::runWithVideoSingleFrame(int* X, int* Y) {
-	VideoCapture cap(0); //capture the video from webcam
+	VideoCapture static cap(0); //capture the video from webcam
 
-	if (!_isPhoto) {
-
-		if (!cap.isOpened())  // if not success, exit program
-		{
-			cout << "Cannot open the web cam" << endl;
-			return;
-		}
+	if (!cap.isOpened())  // if not success, exit program
+	{
+		cout << "Cannot open the web cam" << endl;
+		return;
 	}
 
 	//Capture a temporary image from the camera
 	Mat imgTmp;
-	if (_isPhoto) {
-		imgTmp = imread(filename);
-		resize(imgTmp, imgTmp, imgTmp.size() / 5);
-	}
-	else {
-		cap.read(imgTmp);
-	}
+	
+	cap.read(imgTmp);
 
 	//Create a black image with the size as the camera output
 	Mat imgLines = Mat::zeros(imgTmp.size(), CV_8UC3);;
 
-		Mat imgOriginal;
+	Mat imgOriginal;
 
-		if (_isPhoto) {
-			imgOriginal = imread(filename);
-			resize(imgOriginal, imgOriginal, imgOriginal.size() / 5);
+	cap.read(imgOriginal); // read a new frame from video
+	bool bSuccess = !imgOriginal.empty();
+
+
+	if (!bSuccess) //if not success, break loop
+	{
+		cout << "Cannot read a frame from video stream" << endl;
+		return;
+	}
+
+	Mat imgHSV;
+
+	cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
+
+	Mat imgThresholded, imgThresholded2;
+
+	inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV),
+		imgThresholded); //Threshold the image
+	inRange(imgHSV, Scalar(iLowH2, iLowS, iLowV), Scalar(iHighH2, iHighS, iHighV),
+		imgThresholded2);
+
+	imgThresholded = imgThresholded | imgThresholded2;
+
+	
+	//morphological opening (removes small objects from the foreground)
+	erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+	dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+
+	//morphological closing (removes small holes from the foreground)
+	dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+	erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+	
+
+	//Calculate the moments of the thresholded image
+	Moments oMoments = moments(imgThresholded);
+
+	double dM01 = oMoments.m01;
+	double dM10 = oMoments.m10;
+	double dArea = oMoments.m00;
+
+	// if the area <= 10000, I consider that the there are no object in the image and it's because of the noise, the area is not zero
+	if (dArea > 10000) {
+		//calculate the position of the ball
+		int posX = dM10 / dArea;
+		int posY = dM01 / dArea;
+
+		/*
+		if (iLastX >= 0 && iLastY >= 0 && posX >= 0 && posY >= 0) {
+			//Draw a red line from the previous point to the current point
+			line(imgLines, Point(posX, posY), Point(iLastX, iLastY), Scalar(0, 0, 255), 2);
 		}
-		else {
-			bool bSuccess = cap.read(imgOriginal); // read a new frame from video
-		}
-		bool bSuccess = !imgOriginal.empty();
+		*/
 
+		iLastX = posX;
+		iLastY = posY;
+	}
 
-		if (!bSuccess) //if not success, break loop
-		{
-			cout << "Cannot read a frame from video stream" << endl;
-			return;
-		}
+	*X = iLastX;
+	*Y = iLastY;
 
-		Mat imgHSV;
+	vector<Mat> channels;
+	split(imgThresholded, channels);
 
-		cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
-
-		Mat imgThresholded, imgThresholded2;
-
-		inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV),
-			imgThresholded); //Threshold the image
-		inRange(imgHSV, Scalar(iLowH2, iLowS, iLowV), Scalar(iHighH2, iHighS, iHighV),
-			imgThresholded2);
-
-		imgThresholded = imgThresholded | imgThresholded2;
-
-		//morphological opening (removes small objects from the foreground)
-		erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
-		dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
-
-		//morphological closing (removes small holes from the foreground)
-		dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
-		erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
-
-		//Calculate the moments of the thresholded image
-		Moments oMoments = moments(imgThresholded);
-
-		double dM01 = oMoments.m01;
-		double dM10 = oMoments.m10;
-		double dArea = oMoments.m00;
-
-		// if the area <= 10000, I consider that the there are no object in the image and it's because of the noise, the area is not zero
-		if (dArea > 10000) {
-			//calculate the position of the ball
-			int posX = dM10 / dArea;
-			int posY = dM01 / dArea;
-
-			if (iLastX >= 0 && iLastY >= 0 && posX >= 0 && posY >= 0) {
-				//Draw a red line from the previous point to the current point
-				line(imgLines, Point(posX, posY), Point(iLastX, iLastY), Scalar(0, 0, 255), 2);
-			}
-
-			iLastX = posX;
-			iLastY = posY;
-		}
-
-		*X = iLastX;
-		*Y = iLastY;
-
-		vector<Mat> channels;
-		split(imgThresholded, channels);
-
-		Mat red;
-		inRange(channels[0], Scalar(0), Scalar(10), red); // red
+	Mat red;
+	inRange(channels[0], Scalar(0), Scalar(10), red); // red
 	// ... do the same for blue, green, etc only changing the Scalar values and the Mat
 
-		double image_size = imgThresholded.cols * imgThresholded.rows;
-		double red_percent = (1 - ((double)cv::countNonZero(red)) / image_size) * 100;
+	double image_size = imgThresholded.cols * imgThresholded.rows;
+	printf("im_size: %lf", image_size);
+	double red_percent = (1 - ((double)cv::countNonZero(red)) / image_size) * 100;
 
-		//cout << "Red percent: " << red_percent << "%" << endl;
-		if (red_percent > 20) cout << "BANG!!" << endl;
+	//cout << "Red percent: " << red_percent << "%" << endl;
+	if (red_percent > 20) cout << "BANG!!" << endl;
+	
+	/*
+	//to ma dodac aktualna ilosc w % czerwonego na zarejestrowanym obrazie ale trzeba naprawic
+	putText(imgThresholded, to_string(red_percent), Point2f(100, 100), FONT_HERSHEY_PLAIN, 2, Scalar(0, 0, 255, 255));
 
-		//to ma dodac aktualna ilosc w % czerwonego na zarejestrowanym obrazie ale trzeba naprawic
-		putText(imgThresholded, to_string(red_percent), Point2f(100, 100), FONT_HERSHEY_PLAIN, 2, Scalar(0, 0, 255, 255));
+	Mat flipHorizontal, flipOriginalHorizontal;
+	flip(imgThresholded, flipHorizontal, 1);
+	imshow("Thresholded Image", flipHorizontal); //show the thresholded image
 
-		Mat flipHorizontal, flipOriginalHorizontal;
-		flip(imgThresholded, flipHorizontal, 1);
-		imshow("Thresholded Image", flipHorizontal); //show the thresholded image
+	imgOriginal = imgOriginal + imgLines;
 
-		imgOriginal = imgOriginal + imgLines;
-
-		flip(imgOriginal, flipOriginalHorizontal, 1);
-		imshow("Original", flipOriginalHorizontal); //show the original image
+	flip(imgOriginal, flipOriginalHorizontal, 1);
+	imshow("Original", flipOriginalHorizontal); //show the original image
+	*/
 }
