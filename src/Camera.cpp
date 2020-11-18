45857d28571206e72fb04d579cc1a2c62b3717a6
Patryk Orlowski <222530@edu.p.lodz.pl>
2020-11-18 16:35:08 +0100

    Changed .h include to .hpp include

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
-#include "Camera.h"
+#include "Camera.hpp"
 
 using namespace std;
 using namespace cv;
